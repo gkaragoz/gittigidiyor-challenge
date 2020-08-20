d21c4265d694e9cda1aad135d320133e3cb092c8
gkaragoz <gkaragoz96@gmail.com>
2020-08-20 19:26:44 +0300

    Setup apollo-client and request query.

---
+++ client/src/api/queries.js
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const getProductsQuery = gql`
+  query GetProducts {
+    products {
+      id
+      title
+      price
+      currency
+      imageURL
+      shipping
+    }
+  }
+`;
